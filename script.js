3b0d4a449ab7b7828b06d363aa9607a28a9198f2
spencer-m8 <shsmclean@gmail.com>
2026-05-25 20:03:04 -0400

    once you click enough, a new page is opened

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const searchBtn = document.getElementById("searchButton");
 const searchBox = document.getElementById("masthead-search-term");
 
+let timesResized = 0;
+
 searchBox.addEventListener('input', async () => {
     //want to resize the screenshot thing by the length of this
     const crop = 10+ searchBox.value.length * 4;
@@ -11,10 +13,20 @@ searchBox.addEventListener('input', async () => {
     const canvas = await html2canvas(searchBtn);
     ctx.drawImage(canvas, crop, crop, crop, crop, 0, 0, crop, crop);
     searchBtn.replaceChildren(canvas);
+    timesResized++;
 });
 
 searchBtn.addEventListener('click', async () => {
             const canvas = await html2canvas(searchBtn);
             searchBtn.appendChild(canvas);
+            timesResized++;
+            console.log(timesResized);
+
+            if (timesResized > 10) {
+                alert("Search complete.")
+                window.open("./enter.html")
+            }
 });
+
+
 
